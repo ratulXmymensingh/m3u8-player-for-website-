515d2a116bdb920b6cb07de773642eb3507145b9
Ratul Hasan <82720031+ratulXmymensingh@users.noreply.github.com>
2024-07-06 04:24:30 +0600

    Hls js Latest Version.js
    Copy link dependencies or more to player

---
+++ Hls Latest Version.js
@@ -0,0 +1,19 @@
+
+{
+  "name": "home",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "dependencies": {
+        "hls.js": "^1.5.13"
+      }
+    },
+    "node_modules/hls.js": {
+      "version": "1.5.13",
+      "resolved": "https://registry.npmjs.org/hls.js/-/hls.js>
+      "integrity": "sha512-xRgKo84nsC7clEvSfIdgn/Tc0NOT+d7vdi>
+      "license": "Apache-2.0"
+    }
+  }
+} 
